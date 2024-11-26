2eac91024ba98d0d193bcc3f7411340410947886
bnaod <z475856982@gmail.com>
2024-11-26 21:20:53 +0800

    20241126

---
+++ gitPush.bat
@@ -0,0 +1,30 @@
+@echo off
+ 
+title GIT一键提交
+color 3
+cd D:\hugo_0.82.1_Windows-64bit\www3
+hugo
+cd D:\hugo_0.82.1_Windows-64bit\www3\public
+echo 当前目录是：%cd%
+echo;
+ 
+echo 开始添加变更：git add .
+git add .
+echo;
+ 
+set /p declation=输入提交的commit信息:
+git commit -m "%declation%"
+echo;
+ 
+echo 执行git pull
+git pull
+echo;
+ 
+echo 执行git push origin master
+git push origin master
+echo;
+ 
+echo 执行完毕！
+echo;
+ 
+pause
